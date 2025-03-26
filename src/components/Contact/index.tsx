eae5bee6a66fa598c6c72ffd883e297197eefea4
Simbarashe Kuwanda <stkuwanda@gmail.com>
2025-03-26 15:16:02 +0200

    implemented contact us form integration with emailjs

--- src/components/Contact/index.tsx
+++ src/components/Contact/index.tsx
@@ -1,11 +1,47 @@
+"use client";
+import { useState, useRef } from 'react';
+import emailjs from '@emailjs/browser';
+
 import NewsLatterBox from "./NewsLatterBox";
+import { emailjsPublicKey, emailjsServiceId, emailjsTemplateId } from '@/services/access';
 
 const Contact = () => {
+  const [btnText, setBtnText] = useState<string>('Submit');
+  const [isDisabled, setIsDisabled] = useState<boolean>(false);
+  const form = useRef<HTMLFormElement>(null);
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+    setBtnText('Sending...');
+    setIsDisabled(true);
+
+    emailjs
+      .sendForm(emailjsServiceId, emailjsTemplateId, form.current, {
+        publicKey: emailjsPublicKey,
+      })
+      .then(
+        () => {
+          console.log('SUCCESS!');
+          form.current.reset();
+          setBtnText('Success');
+        },
+        (error) => {
+          console.log('FAILED...', error.text);
+          setBtnText('Failed!');
+        },
+      ).finally(() => {
+        setTimeout(() => {
+          setBtnText('Submit');
+          setIsDisabled(false);
+        }, 3000);
+      });
+  };
+
   return (
     <section id="contact" className="overflow-hidden py-16 md:py-20 lg:py-28">
       <div className="container">
         <div className="-mx-4 flex flex-wrap">
-          <div className="w-full px-4 lg:w-7/12 xl:w-8/12">
+          <div className="w-full px-4 ">
             <div
               className="mb-12 rounded-sm bg-white px-8 py-11 shadow-three dark:bg-gray-dark sm:p-[55px] lg:mb-5 lg:px-8 xl:p-[55px]"
               data-wow-delay=".15s
@@ -17,7 +53,7 @@ const Contact = () => {
               {/* <p className="mb-12 text-base font-medium text-body-color">
                 Our support team will get back to you ASAP via email.
               </p> */}
-              <form>
+              <form ref={form} onSubmit={sendEmail}>
                 <div className="-mx-4 flex flex-wrap">
                   <div className="w-full px-4 md:w-1/2">
                     <div className="mb-8">
@@ -28,7 +64,9 @@ const Contact = () => {
                         Your Name
                       </label>
                       <input
+                        name="name"
                         type="text"
+                        required
                         placeholder="Enter your name"
                         className="border-stroke w-full rounded-sm border bg-[#f8f8f8] px-6 py-3 text-base text-body-color outline-none focus:border-primary dark:border-transparent dark:bg-[#2C303B] dark:text-body-color-dark dark:shadow-two dark:focus:border-primary dark:focus:shadow-none"
                       />
@@ -43,7 +81,9 @@ const Contact = () => {
                         Your Email
                       </label>
                       <input
+                        name="email"
                         type="email"
+                        required
                         placeholder="Enter your email"
                         className="border-stroke w-full rounded-sm border bg-[#f8f8f8] px-6 py-3 text-base text-body-color outline-none focus:border-primary dark:border-transparent dark:bg-[#2C303B] dark:text-body-color-dark dark:shadow-two dark:focus:border-primary dark:focus:shadow-none"
                       />
@@ -58,6 +98,7 @@ const Contact = () => {
                         Your Phone
                       </label>
                       <input
+                        name="phone_number"
                         type="tel"
                         placeholder="Enter your phone number"
                         className="border-stroke w-full rounded-sm border bg-[#f8f8f8] px-6 py-3 text-base text-body-color outline-none focus:border-primary dark:border-transparent dark:bg-[#2C303B] dark:text-body-color-dark dark:shadow-two dark:focus:border-primary dark:focus:shadow-none"
@@ -74,6 +115,7 @@ const Contact = () => {
                       </label>
                       <textarea
                         name="message"
+                        required
                         rows={5}
                         placeholder="Enter your Message"
                         className="border-stroke w-full resize-none rounded-sm border bg-[#f8f8f8] px-6 py-3 text-base text-body-color outline-none focus:border-primary dark:border-transparent dark:bg-[#2C303B] dark:text-body-color-dark dark:shadow-two dark:focus:border-primary dark:focus:shadow-none"
@@ -81,17 +123,17 @@ const Contact = () => {
                     </div>
                   </div>
                   <div className="w-full px-4">
-                    <button className="rounded-sm bg-primary px-9 py-4 text-base font-medium text-white shadow-submit duration-300 hover:bg-primary/90 dark:shadow-submit-dark">
-                      Submit Ticket
+                    <button disabled={isDisabled} className="rounded-sm bg-primary px-9 py-4 text-base font-medium text-white shadow-submit duration-300 hover:bg-primary/90 dark:shadow-submit-dark">
+                      {btnText}
                     </button>
                   </div>
                 </div>
               </form>
             </div>
           </div>
-          <div className="w-full px-4 lg:w-5/12 xl:w-4/12">
+          {/* <div className="w-full px-4 lg:w-5/12 xl:w-4/12">
             <NewsLatterBox />
-          </div>
+          </div> */}
         </div>
       </div>
     </section>
