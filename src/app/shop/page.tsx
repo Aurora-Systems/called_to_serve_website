93e2be8f7cd06ce8bd5d762f3dd56b4e5f0185dc
Simbarashe Kuwanda <stkuwanda@gmail.com>
2025-03-27 12:03:15 +0200

    updated card and shop page styling

--- src/app/shop/page.tsx
+++ src/app/shop/page.tsx
@@ -16,22 +16,8 @@ const Shop = () => {
         pageName="Shop"
         description="Explore our catalogue."
       />
-      {/* <section className="pb-[150px] pt-[150px] bg-[url(https://ngratesc.sirv.com/called_to_serve/carpart.png)]">
-        <div className="container">
-          <div className="-mx-4 flex flex-wrap justify-center">
-            <ShoppingItems />
-          </div>
-        </div>
-      </section> */}
-    
-      {/* <section className="flex items-center justify-center container mx-auto pb-10 pt-10">
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
-          <ShoppingItems />
-        </div>
-      </section> */}
       
-          <ShoppingItems />
- 
+      <ShoppingItems />
     </>
   );
 }
--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -3,16 +3,17 @@
 function Card(props) {
   return (
     <div className="card">
-      <div className="p-5 flex flex-col">
+      <div className="p-5 flex flex-col justify-between h-full">
         <div className="rounded-xl overflow-hidden">
-          <img src={props.imageUrl} alt="" />
+          <img src={props.imageUrl} alt="image of a shopping item" style={{maxHeight: '200px'}} />
         </div>
         <div className="flex items-center justify-between">
           <h5 className="text-2xl md:text-3xl font-medium mt-3">{props.itemName}</h5>
           <h5 className="text-2xl md:text-3xl font-medium mt-3 text-primary">${props.price}</h5>
         </div>
-        <p className="text-lg mt-3">{props.stockMessage}</p>
-        <a href={`https://wa.me/263786955677?text=Good day, I would like to get more information on this product - ${props.itemName}  going for $ ${props.price} USD`} className="text-center bg-red-400 text-red-700 py-2 rounded-lg font-semibold mt-4 hover:bg-red-300 focus:scale-95 transition-all duration-200 ease-out">Buy</a>
+       
+        <p className="text-lg mt-3 mb-auto">{props.stockMessage}</p>
+        <a href={`https://wa.me/263786955677?text=Good day, I would like to get more information on this product - ${props.itemName}  going for $ ${props.price} USD`} className="text-center bg-red-400 text-red-700 py-2 rounded-lg font-semibold hover:bg-red-300 focus:scale-95 transition-all duration-200 ease-out">Buy</a>
       </div>
     </div>
   )
