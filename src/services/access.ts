9dbb3b257226ecf98a50ae39d685207e45fcf5dd
Simbarashe Kuwanda <stkuwanda@gmail.com>
2025-03-28 10:37:27 +0200

    updated userId db connection string

--- src/services/access.ts
+++ src/services/access.ts
@@ -1,4 +1,4 @@
-export const userId: string = "kp_06ac0cea44c8446d858b31bc6888234b";
+export const userId: string = "kp_179567599ef14cea9aea21b19496b566";
 export const db_url: string = "https://wydajmnykbvjicgnncqz.supabase.co";
 export const key: string =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Ind5ZGFqbW55a2J2amljZ25uY3F6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mjc3MTA1NDgsImV4cCI6MjA0MzI4NjU0OH0.DdyT8s6m_6cVmvJq0-KsV1y70rg2dC9KX6v4rzEcjro";
